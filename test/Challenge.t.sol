7d0ebd107430b6a44dfbe5b63fce62ae6ff6282c
clabby <ben@clab.by>
2023-04-20 10:10:04 -0400

    push

--- test/Challenge.t.sol
+++ test/Challenge.t.sol
@@ -22,14 +22,16 @@ contract ChallengeTest is Test {
 
     // @dev Tests that the `name` function works as expected.
     function testName() public {
-        assertEq(puzzle.name(), "FizzBuzz");
+        assertEq(puzzle.name(), "Antikythera");
     }
 
     /// @dev Tests that the `generate` function works as expected.
-    function testFuzz_generate_succeeds(address _seed) public {
-        assertEq(puzzle.generate(_seed), uint256(keccak256(abi.encode(_seed))));
+    function testFuzz_generate_succeeds() public {
+        address _seed = ZERO_COOL;
+        assertEq(puzzle.generate(_seed), uint256(keccak256(abi.encode(_seed, 0x7dffffffffff9b7c))));
     }
 
+    // b4316a5
     function testVerify_solution() public {
         // TODO: Impl
         vm.startPrank(ZERO_COOL);
@@ -37,22 +39,36 @@ contract ChallengeTest is Test {
         uint256 solution;
 
         // Phase 1.a
-        solution |= (start & (0xFF << (0x1F - 0x12) * 8)) >> 0x07 * 8;
+        solution |= (start & (0xFF << offset(0x12))) << 0x02 * 8;
 
         // Phase 1.b
-        uint256 mask = 0x0F << (0x1F - 0x19) * 8;
+        uint256 mask = 0xFF << offset(0x10);
         solution &= ~mask;
-        solution |= (0x05 << (0x1F - 0x19) * 8);
+        solution |= (0x26 << offset(0x10));
 
+        emit log_bytes32(bytes32(start));
         // Phase 2
-        // 0xc0 << 16 + 0xff << 8 + 0xee
-        solution |= 0x03c01003ff0803ee00FF << (0x1F - 0x09) * 8;
+        // 0xc0 << 67 + 0x53 << 8 + 0xb5 << 0
+        uint256 bytecode = 0x030010030008030000FF00000000000000000000000000000000000000000000;
+        bytecode |= ((start & 0xFF0000) >> 0x10) << offset(0x01);
+        bytecode |= ((start & 0xFF00) >> 0x08) << offset(0x04);
+        bytecode |= (start & 0xFF) << offset(0x07);
+        solution |= bytecode;
 
         // Phase 3
+        solution |= 0x0200;
         
+        // Phase 4
+
+        // Final
+        solution |= 0x01;
         
         emit log_string(string.concat("solution: ", vm.toString(bytes32(solution))));
         assertTrue(puzzle.verify(start, solution));
         vm.stopPrank();
+    }
+
+    function offset(uint8 _length) internal pure returns (uint8 _bits) {
+        _bits = (0x1f - _length) * 8;
     }
 }
